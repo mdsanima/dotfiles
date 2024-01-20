6c3bb010bd3b34735608e893f2cc88e482307f59
Marcin Różewski <marcinrozewski@gmail.com>
2024-01-20 18:29:22 +0100

    refactor: Moved help printing function to `mdsanima-prints` file (#29)

--- install.sh
+++ install.sh
@@ -14,62 +14,9 @@ APT_PACKAGES_OPTIONAL="curl git htop vim tmux mc neofetch cmatrix ffmpeg"
 # Get the current tag from the Git repository
 CURRENT_GIT_TAG=$(git describe --tags)
 
-# Import default colors definition
-source "$PWD/./local/bin/mdsanima-colors"
-
-
-# Help function for printing text in colors
-function _color_print() {
-    local text="$1"
-    local fg_color
-    local bg_color
-    local no_newline=false
-
-    shift 1
-
-    while [[ $# -gt 0 ]]; do
-        case "$1" in
-            -fg)
-                fg_color="$2"
-                shift 2
-                ;;
-            -bg)
-                bg_color="$2"
-                shift 2
-                ;;
-            -nonewline)
-                no_newline=true
-                shift 1
-                ;;
-            *)
-                echo "unrecognized argument: $1"
-                return 1
-                ;;
-        esac
-    done
-
-    local fg_code
-    local bg_code
-    local reset_code="\x1b[0m"
-
-    if [[ "$fg_color" =~ ^[0-9]+$ ]]; then
-        fg_code="\x1b[38;5;${fg_color}m"
-    elif [[ -v "$fg_color" ]]; then
-        fg_code="\x1b[38;5;${!fg_color}m"
-    fi
-
-    if [[ "$bg_color" =~ ^[0-9]+$ ]]; then
-        bg_code="\x1b[48;5;${bg_color}m"
-    elif [[ -v "$bg_color" ]]; then
-        bg_code="\x1b[48;5;${!bg_color}m"
-    fi
-
-    if [[ "$no_newline" == true ]]; then
-        echo -e -n "${fg_code}${bg_code}${text}${reset_code}"
-    else
-        echo -e "${fg_code}${bg_code}${text}${reset_code}"
-    fi
-}
+# Import default colors definition and help function for printing text in colors
+source "$PWD/.local/bin/mdsanima-colors"
+source "$PWD/.local/bin/mdsanima-prints"
 
 
 # Event logs information
