103ac9e64bb6f76e32a7b5947f36fb24889c8abd
mdsanima <marcinrozewski@gmail.com>
2024-01-27 20:57:23 +0100

    refactor: The `mdsanima-shell` module and clean up
    Streamlined the `mdsanima-shell` module by removing redundant scripts
    and consolidating functionality into a simplified entry point. Legacy
    scripts for banners, help, and weather retrieval were replaced with more
    cohesive and maintainable versions within a newly structured module
    directory. Unused placeholder files within the private and public
    subdirectories were also purged.
    
    The refactoring aims to improve development speed, readability, and ease
    of use for the mdsanima development environment. Modules related to
    color printing, prompts, and weather are now appropriately organized,
    promoting better code practices and preparing for future expansion or
    updates.
    
    The commit also includes updates to the module's documentation to
    reflect these changes and ensure alignment with the current
    functionality and project layout.

--- modules/mdsanima-shell/README.md
+++ modules/mdsanima-shell/README.md
@@ -1,5 +1,5 @@
 # mdsanima-shell
 
-This is the `mdsanima-shell` module for _Shell_ terminal on _Linux_ system.
+This is the documentation for the `mdsanima-shell` module for terminal _Shell Script_ on _Linux_ system.
 
 The commands in this module are used to manipulate the power state of the computer.
---
+++ modules/mdsanima-shell/private/weather.sh
@@ -0,0 +1,9 @@
+# Copyright (c) 2023-2024 MDSANIMA DEV. All rights reserved.
+# Licensed under the MIT license.
+
+# Check the weather in your nearest location or by target city and show the weather on the terminal in nice way.
+
+
+__mdsanima_get_actual_weather() {
+    curl "https://wttr.in/${1:-}"
+}
