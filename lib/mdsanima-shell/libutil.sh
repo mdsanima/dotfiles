de2072225e952248e296b2a4581e607c2eec6485
Marcin Różewski <marcinrozewski@gmail.com>
2024-02-09 20:03:20 +0100

    feat: Add utility functions for shell operations
    Introduced a new utility script containing functions to enhance
    shell scripting capabilities.
    
    The `util::one_line_progress` function enables concise progress
    output by overwriting lines in the console, useful for updating
    status in-place during long-running commands.
    
    The `util::check_package_is_installed` function simplifies package
    verification by returning status based on the installed state
    of a given package.
    
    These enhancements aim to streamline command-line operations and
    scripting clarity within our shell environments.

---
+++ lib/mdsanima-shell/libutil.sh
@@ -0,0 +1,57 @@
+# Copyright (c) 2024 MDSANIMA DEV. All rights reserved.
+# Licensed under the MIT license.
+
+# This library is for utility functions.
+
+
+# Cleaned line sequence
+declare clean_line_seq="\r\e[0K"
+
+
+#-------------------------------------------------------------------------------
+# This function provides a simple way to execute bash commands with one line in
+# the stdout progress by reading each line of the stdout and displaying it with
+# overwriting the previous line.
+#
+# Arguments:
+#   <command>  The command to be executed, required
+#
+# Usage:
+#   util::one_line_progress <command>
+#   util::one_line_progress sudo apt update
+#-------------------------------------------------------------------------------
+function util::one_line_progress() {
+  local command="$@"
+
+  # Execute the command and read each line of the stdout
+  ${command} 2>&1 | while IFS= read -r line; do
+    echo -n -e "${clean_line_seq}${line}"
+  done
+}
+
+
+#-------------------------------------------------------------------------------
+# This function provides a simple way to check if a package is installed.
+#
+# Arguments:
+#   <package>  The package name to check if is installed, required
+#
+# Returns:
+#   0 if the package is installed
+#   1 if the package is not installed
+#
+# Usage:
+#   util::check_package_is_installed <package>
+#   util::check_package_is_installed git
+#-------------------------------------------------------------------------------
+function util::check_package_is_installed() {
+  local package="$1"
+  local query=$(dpkg-query -W -f='${Status}' "${package}" 2>/dev/null)
+
+  # Check if the package is installed
+  if echo "${query}" | grep -q "install ok installed"; then
+    return 0
+  else
+    return 1
+  fi
+}
