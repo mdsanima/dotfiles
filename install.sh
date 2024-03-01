c20ee50c7160e32a5f70772b6002e7dd18fbcfb6
Marcin Różewski <marcinrozewski@gmail.com>
2024-03-01 20:21:47 +0100

    chore: Enhance `install.sh` robustness and messaging
    The installer script now exits on any error, preventing partial
    installs and encouraging a fail-fast philosophy. We also ensure the
    script isn't run with root privileges to avoid potential system-wide
    side effects. Additionally, event handling has been initialized and
    messaging updated to give users more precise feedback during the
    installation process.
    
    Furthermore, the git tag retrieval has been expanded to include both
    current and latest tags, offering more detailed repository information.
    The installer's visual presentation and branding also received a refresh
    for improved aesthetics and clarity.
    
    In a related update, error messages in libprint.sh have been standardized
    with the addition of punctuation, promoting consistency and improving readability.
    
    These adjustments lead to a more stable and user-friendly installation experience.

--- install.sh
+++ install.sh
@@ -5,23 +5,44 @@
 
 # Installation script for dotfiles for GNU/Linux systems like Debian or Ubuntu.
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
 # Import necessary libraries
 source "$PWD/lib/mdsanima-shell/libcolor.sh"
+source "$PWD/lib/mdsanima-shell/libconvert.sh"
+source "$PWD/lib/mdsanima-shell/libevent.sh"
 source "$PWD/lib/mdsanima-shell/libprint.sh"
 source "$PWD/lib/mdsanima-shell/libutil.sh"
+
+# Initialize event functions
+info=$(event::info)
+error=$(event::error)
+success=$(event::success)
+
+# Check the user
+if [[ "$USER" == "root" ]]; then
+  echo -e "${error} Do not run 'install.sh' script as root." >&2
+  exit 1
+fi
 
 # List of packages to install
 APT_PACKAGES="python3-pip zsh powerline fonts-powerline zsh-theme-powerlevel9k"
 APT_PACKAGES_OPTIONAL="curl git htop vim tmux mc neofetch cmatrix ffmpeg"
 
-# Get the current tag from the Git repository
+# Get tags from the Git repository
 CURRENT_GIT_TAG=$(git describe --tags)
+LATEST_GIT_TAG=$(git describe --tags --abbrev=0)
 
-function dotfiles_installer() {
-  print::color -fg ${WHITE} -bg ${BLUE} -b -n " MDSANIMA DEV "
-  print::color -fg ${BLUE} " dotfiles ${CURRENT_GIT_TAG}\n"
-  print::color -fg ${WHITE} "This installer is only available for GNU/Linux systems like Debian or Ubuntu.\n"
-  print::color -fg ${GRAY} "Copyright (c) 2024 MDSANIMA DEV. All rights reserved. Licensed under the MIT license."
+function dotfiles_installer_info() {
+  print::color -fg ${GRAY} "Copyright (c) 2024 MDSANIMA DEV. All rights reserved. Licensed under the MIT license.\n"
+  print::color -fg ${SKY} -b -n "  MDSANIMA-SHELL"
+  print::color -fg ${BLUE} " dotfiles ${CURRENT_GIT_TAG}"
+  print::color -fg ${ORANGE} "  This installer is only available for GNU/Linux systems like Debian or Ubuntu\n"
 }
 
-dotfiles_installer
+dotfiles_installer_info
+echo -e "${info} Install prerequisites on your system."
+echo "Running apt update ..."
+echo "Running apt upgrade ..."
+echo -e "${success} Configurations for the dotfiles was installed."
