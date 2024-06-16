cc3b4b29c21fd524cd70e4547418ebf4f65c125c
Marcin Różewski <marcinrozewski@gmail.com>
2024-06-16 08:14:35 +0200

    refactor: Improve installation script for PowerShell
    Revamped comments to enhance clarity and consistency in the installation
    script and `mdsanima-power` module. Updated event logs formatting for
    better readability. Adjusted terminology from _folder_ to _directory_ for
    precision. Trimmed gitkeep files in `private` and `public` directories to
    only indicate preservation of structure.
    
    This refactor aims to improve code readability and maintainability without
    altering functionality.

--- mod/mdsanima-power/mdsanima-power.psm1
+++ mod/mdsanima-power/mdsanima-power.psm1
@@ -1,4 +1,5 @@
 # Copyright (c) 2024 MDSANIMA DEV. All rights reserved.
 # Licensed under the MIT license.
 
-# This is the `mdsanima-power` module for PowerShell terminal on Windows system.
+# This is the `mdsanima-power` module for PowerShell terminal on Windows system.  The installation
+# script is still work in progress.
