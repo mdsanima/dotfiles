8bf7569abf74ee911c5198600fd0d6057d9c74ae
Marcin Różewski <marcinrozewski@gmail.com>
2024-09-06 01:27:10 +0200

    fix: Back to default terminal cursor style

--- config/nvim/lua/mdsanima/autocmds.lua
+++ config/nvim/lua/mdsanima/autocmds.lua
@@ -1,4 +1,7 @@
 -- General autocommands.  This is a good place to put command that you want to run on every startup.
 
--- TODO:  Back to default terminal cursor style blinking block when leaving the Neovim editor
--- NOTE:  This is old vim code: autocmd VimLeave * set guicursor=a:block1-blinkon1
+--  Back to default terminal cursor style blinking block when leaving the Neovim editor
+vim.api.nvim_create_autocmd("VimLeave", {
+    pattern = "*",
+    command = "set guicursor=a:block1-blinkon1",
+})
