044e2097f5f9263be2976c535669cc8c72e2dd10
Marcin Różewski <marcinrozewski@gmail.com>
2024-09-27 00:06:28 +0200

    style: Use 80 chars max lines length
    This also update some comment for documentation and format code.

--- .commitlintrc.mjs
+++ .commitlintrc.mjs
@@ -1,12 +1,20 @@
-// Defined rules on commit messages that are checked on pull requests.
+// Maintain a consistent coding style throughout the entire project codebase to
+// ensure readability and maintainability.  Consistent style not only improves
+// collaboration but also makes the code easier to review and debug.
 
-// See the documentation for reference rules: https://commitlint.js.org/#/reference-rules
+// Linter configuration for rules on commit messages that are checked on pull
+// requests, and before push to origin from local reposiotry.  For more
+// information about, refer to the commitlint official documentation for the
+// reference rules, see here: https://commitlint.js.org/#/reference-rules
 
-// These rules are used to check if the commit message contains a type.  The type is the first word
-// in the commit message and must match one of the types defined in the `typesRule` array in this
-// configuration file.
+// The rules are still a work in progress and subject to change.  I still need
+// to think about this configuration, and for now this is a base template.
 
-/** The array of rule types that are allowed in commit messages */
+// These rules are used to check if the commit message contains a type.  The
+// type is the first word in the commit message and must match one of the types
+// defined in the `typesRule` array in this configuration file.
+
+/** The array of type rules that are allowed in commit messages. */
 const typesRule = [
     "build",
     "chore",
@@ -21,10 +29,15 @@ const typesRule = [
     "test",
 ];
 
-/** The array of rule subjects that are allowed in commit messages */
-const subjectsRule = ["upper-case", "pascal-case", "sentence-case", "start-case"];
+/** The array of subject rules that are allowed in commit messages. */
+const subjectsRule = [
+    "upper-case",
+    "pascal-case",
+    "sentence-case",
+    "start-case",
+];
 
-/** The rules configuration for commitlint that is used in GitHub Workflow Actions */
+/** The rules for commitlint that is used in GitHub Workflow Actions on PR. */
 const commitlintConfig = {
     rules: {
         "type-enum": [2, "always", typesRule],
@@ -34,12 +47,12 @@ const commitlintConfig = {
         "subject-case": [2, "always", "sentence-case"],
         "subject-empty": [2, "never"],
         "subject-full-stop": [2, "never", "."],
-        "header-max-length": [2, "always", 80],
+        "header-max-length": [2, "always", 72],
         "body-case": [2, "always", "sentence-case"],
         "body-leading-blank": [2, "always"],
-        "body-max-line-length": [1, "always", 100],
+        "body-max-line-length": [1, "always", 80],
         "footer-leading-blank": [2, "always"],
-        "footer-max-line-length": [1, "always", 100],
+        "footer-max-line-length": [1, "always", 80],
     },
 };
 
