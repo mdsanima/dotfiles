fac0ec7052cfcf1c33cb6999888da54f8f5dc8c1
Marcin Różewski <marcinrozewski@gmail.com>
2024-11-24 00:54:40 +0100

    style: Format Shell script library (#144)

--- lib/mdsanima-shell/libprint.sh
+++ lib/mdsanima-shell/libprint.sh
@@ -1,11 +1,11 @@
-# Copyright (c) 2024 MDSANIMA DEV. All rights reserved.
+# Copyright (c) 2024 Marcin Różewski, MDSANIMA LAB. All rights reserved.
 # Licensed under the MIT license.
 
 # Library for printing functions.  This is still work in progress.
 
-#---------------------------------------------------------------------------------------------------
-# This function provides a simple way to print the text with colors on the terminal bash script
-# outputs.  The color mode may be number, name or HEX code.
+#------------------------------------------------------------------------------
+# This function provides a simple way to print the text with colors on the
+# terminal bash script outputs.  The color mode may be number, name or HEX code.
 #
 # Arguments:
 #   -fg <color>     Foreground color mode, optional, default to none
@@ -22,146 +22,146 @@
 #   print::color -fg ${BLACK} -bg ${INT_RED} "Black text on red background"
 #   print::color -fg ${HEX_YELLOW} -i "Italic yellow text"
 #   print::color -fg "#f97316" -b "Bold orange text"
-#---------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 function print::color() {
-  local fg_color
-  local bg_color
-  local bold_text
-  local italic_text
-  local no_newline
-  local text
-  local hex_mode
+    local fg_color
+    local bg_color
+    local bold_text
+    local italic_text
+    local no_newline
+    local text
+    local hex_mode
 
-  # Set color sequences
-  local fg_seq="\e[38;5;"
-  local bg_seq="\e[48;5;"
-  local reset="\e[0m"
+    # Set color sequences
+    local fg_seq="\e[38;5;"
+    local bg_seq="\e[48;5;"
+    local reset="\e[0m"
 
-  # Black bold error text on red background
-  local _error_="${fg_seq}16;01m${bg_seq}196m ERROR ${reset} "
+    # Black bold error text on red background
+    local _error_="${fg_seq}16;01m${bg_seq}196m ERROR ${reset} "
 
-  # Error messages
-  local number_error="Color number must be between 0 and 255."
-  local hex_error="This is a not valid HEX color."
-  local color_error="Unrecognized color:"
-  local argument_error="Unrecognized argument:"
-  local no_text_error="No text specified for coloring."
+    # Error messages
+    local number_error="Color number must be between 0 and 255."
+    local hex_error="This is a not valid HEX color."
+    local color_error="Unrecognized color:"
+    local argument_error="Unrecognized argument:"
+    local no_text_error="No text specified for coloring."
 
-  # Default values
-  bold_text=false
-  italic_text=false
-  no_newline=false
+    # Default values
+    bold_text=false
+    italic_text=false
+    no_newline=false
 
-  function set_color_mode() {
-    local color_mode="$1"
-    set -e
-    if util::is_integer "$color_mode"; then
-      if [[ "$color_mode" -gt 255 ]]; then
-        echo -e "${_error_}${number_error}" >&2
-        return 1 # ERROR
-      else
-        hex_mode=false
-        return 0 # SUCCESS
-      fi
-    elif util::is_first_char_special "$color_mode"; then
-      if [[ "$color_mode" =~ ^#[0-9a-fA-F]{6}$ ]]; then
-        hex_mode=true
-        return 0 # SUCCESS
-      else
-        echo -e "${_error_}${hex_error}" >&2
-        return 1 # ERROR
-      fi
-    else
-      echo -e "${_error_}${color_error} $color_mode" >&2
-      return 1 # ERROR
-    fi
-  }
+    function set_color_mode() {
+        local color_mode="$1"
+        set -e
+        if util::is_integer "$color_mode"; then
+            if [[ "$color_mode" -gt 255 ]]; then
+                echo -e "${_error_}${number_error}" >&2
+                return 1 # ERROR
+            else
+                hex_mode=false
+                return 0 # SUCCESS
+            fi
+        elif util::is_first_char_special "$color_mode"; then
+            if [[ "$color_mode" =~ ^#[0-9a-fA-F]{6}$ ]]; then
+                hex_mode=true
+                return 0 # SUCCESS
+            else
+                echo -e "${_error_}${hex_error}" >&2
+                return 1 # ERROR
+            fi
+        else
+            echo -e "${_error_}${color_error} $color_mode" >&2
+            return 1 # ERROR
+        fi
+    }
 
-  # Parse arguments
-  while [[ $# -gt 0 ]]; do
-    case "$1" in
-    -fg)
-      set_color_mode $2
-      if [[ "$hex_mode" == true ]]; then
-        fg_seq="\e[38;2;"
-        fg_color=$(convert::hex_to_rgb $2)
-      else
-        fg_color="$2"
-      fi
-      shift 2
-      ;;
-    -bg)
-      set_color_mode $2
-      if [[ "$hex_mode" == true ]]; then
-        bg_seq="\e[48;2;"
-        bg_color=$(convert::hex_to_rgb $2)
-      else
-        bg_color="$2"
-      fi
-      shift 2
-      ;;
-    -bold | -b)
-      bold_text=true
-      shift 1
-      ;;
-    -italic | -i)
-      italic_text=true
-      shift 1
-      ;;
-    -nonewline | -n)
-      no_newline=true
-      shift 1
-      ;;
-    *)
-      if [[ -z "${text}" ]]; then
-        text="$1"
-      else
-        echo -e "${_error_}${argument_error} $1" >&2
+    # Parse arguments
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+        -fg)
+            set_color_mode $2
+            if [[ "$hex_mode" == true ]]; then
+                fg_seq="\e[38;2;"
+                fg_color=$(convert::hex_to_rgb $2)
+            else
+                fg_color="$2"
+            fi
+            shift 2
+            ;;
+        -bg)
+            set_color_mode $2
+            if [[ "$hex_mode" == true ]]; then
+                bg_seq="\e[48;2;"
+                bg_color=$(convert::hex_to_rgb $2)
+            else
+                bg_color="$2"
+            fi
+            shift 2
+            ;;
+        -bold | -b)
+            bold_text=true
+            shift 1
+            ;;
+        -italic | -i)
+            italic_text=true
+            shift 1
+            ;;
+        -nonewline | -n)
+            no_newline=true
+            shift 1
+            ;;
+        *)
+            if [[ -z "${text}" ]]; then
+                text="$1"
+            else
+                echo -e "${_error_}${argument_error} $1" >&2
+                return 1
+            fi
+            shift 1
+            ;;
+        esac
+    done
+
+    # Error if no text
+    if [[ -z "${text}" ]]; then
+        echo -e "${_error_}${no_text_error}" >&2
         return 1
-      fi
-      shift 1
-      ;;
-    esac
-  done
+    fi
 
-  # Error if no text
-  if [[ -z "${text}" ]]; then
-    echo -e "${_error_}${no_text_error}" >&2
-    return 1
-  fi
+    # Set foreground
+    local fg_code
+    if util::is_integer $fg_color || util::contains_special_char $fg_color; then
+        fg_code="${fg_seq}${fg_color}m"
+    elif [[ -v "${fg_color}" ]]; then
+        fg_code="${fg_seq}${!fg_color}m"
+    fi
 
-  # Set foreground
-  local fg_code
-  if util::is_integer $fg_color || util::contains_special_char $fg_color; then
-    fg_code="${fg_seq}${fg_color}m"
-  elif [[ -v "${fg_color}" ]]; then
-    fg_code="${fg_seq}${!fg_color}m"
-  fi
+    # Set background
+    local bg_code
+    if util::is_integer $bg_color || util::contains_special_char $bg_color; then
+        bg_code="${bg_seq}${bg_color}m"
+    elif [[ -v "${bg_color}" ]]; then
+        bg_code="${bg_seq}${!bg_color}m"
+    fi
 
-  # Set background
-  local bg_code
-  if util::is_integer $bg_color || util::contains_special_char $bg_color; then
-    bg_code="${bg_seq}${bg_color}m"
-  elif [[ -v "${bg_color}" ]]; then
-    bg_code="${bg_seq}${!bg_color}m"
-  fi
+    # Set bold
+    local bold_seq
+    if [[ "${bold_text}" == true ]]; then
+        bold_seq="\e[1m"
+    fi
 
-  # Set bold
-  local bold_seq
-  if [[ "${bold_text}" == true ]]; then
-    bold_seq="\e[1m"
-  fi
+    # Set italic
+    local italic_seq
+    if [[ "${italic_text}" == true ]]; then
+        italic_seq="\e[3m"
+    fi
 
-  # Set italic
-  local italic_seq
-  if [[ "${italic_text}" == true ]]; then
-    italic_seq="\e[3m"
-  fi
-
-  # Print text with colors
-  if [[ "${no_newline}" == true ]]; then
-    echo -e -n "${fg_code}${bold_seq}${italic_seq}${bg_code}${text}${reset}"
-  else
-    echo -e "${fg_code}${bold_seq}${italic_seq}${bg_code}${text}${reset}"
-  fi
+    # Print text with colors
+    if [[ "${no_newline}" == true ]]; then
+        echo -e -n "${fg_code}${bold_seq}${italic_seq}${bg_code}${text}${reset}"
+    else
+        echo -e "${fg_code}${bold_seq}${italic_seq}${bg_code}${text}${reset}"
+    fi
 }
--- lib/mdsanima-shell/libutil.sh
+++ lib/mdsanima-shell/libutil.sh
@@ -1,4 +1,4 @@
-# Copyright (c) 2024 MDSANIMA DEV. All rights reserved.
+# Copyright (c) 2024 Marcin Różewski, MDSANIMA LAB. All rights reserved.
 # Licensed under the MIT license.
 
 # Library for utility functions.  This is still work in progress.
@@ -6,115 +6,115 @@
 readonly clean_line_seq="\r\e[0K"
 
 function util::is_package_installed() {
-  local package="$1"
-  local query=$(dpkg-query -W -f='${Status}' "${package}" 2>/dev/null)
-  if echo "${query}" | grep -q "install ok installed"; then
-    # echo "SUCCESS $package is installed"
-    return 0
-  else
-    # echo "ERROR $package is not installed"
-    return 1
-  fi
+    local package="$1"
+    local query=$(dpkg-query -W -f='${Status}' "${package}" 2>/dev/null)
+    if echo "${query}" | grep -q "install ok installed"; then
+        # echo "SUCCESS $package is installed"
+        return 0
+    else
+        # echo "ERROR $package is not installed"
+        return 1
+    fi
 }
 
 function util::is_installed() {
-  local package="$1"
-  util::is_package_installed "$package"
+    local package="$1"
+    util::is_package_installed "$package"
 }
 
 function util::is_integer() {
-  local argument="$1"
-  if [[ "$argument" =~ ^[0-9]+$ ]]; then
-    # echo "SUCCESS $argument is a integer"
-    return 0
-  else
-    # echo "ERROR $argument is not a integer"
-    return 1
-  fi
+    local argument="$1"
+    if [[ "$argument" =~ ^[0-9]+$ ]]; then
+        # echo "SUCCESS $argument is a integer"
+        return 0
+    else
+        # echo "ERROR $argument is not a integer"
+        return 1
+    fi
 }
 
 function util::is_float() {
-  local argument="$1"
-  if [[ "$argument" =~ ^[0-9]+\.[0-9]+$ ]]; then
-    # echo "SUCCESS $argument is a float"
-    return 0
-  else
-    # echo "ERROR $argument is not a float"
-    return 1
-  fi
+    local argument="$1"
+    if [[ "$argument" =~ ^[0-9]+\.[0-9]+$ ]]; then
+        # echo "SUCCESS $argument is a float"
+        return 0
+    else
+        # echo "ERROR $argument is not a float"
+        return 1
+    fi
 }
 
 function util::is_number() {
-  local argument="$1"
-  if util::is_integer "$argument" || util::is_float "$argument"; then
-    # echo "SUCCESS $argument is a number"
-    return 0
-  else
-    # echo "ERROR $argument is not a number"
-    return 1
-  fi
+    local argument="$1"
+    if util::is_integer "$argument" || util::is_float "$argument"; then
+        # echo "SUCCESS $argument is a number"
+        return 0
+    else
+        # echo "ERROR $argument is not a number"
+        return 1
+    fi
 }
 
 function util::is_string() {
-  local argument="$1"
-  if [[ "$argument" =~ ^[a-zA-Z]+$ ]]; then
-    # echo "SUCCESS $argument is a string"
-    return 0
-  else
-    # echo "ERROR $argument is not a string"
-    return 1
-  fi
+    local argument="$1"
+    if [[ "$argument" =~ ^[a-zA-Z]+$ ]]; then
+        # echo "SUCCESS $argument is a string"
+        return 0
+    else
+        # echo "ERROR $argument is not a string"
+        return 1
+    fi
 }
 
 function util::is_special_char() {
-  local argument="$1"
-  local special_chars="!@#$%^&*()_+-=[]{}|;:,.<>/?~'\"\\\ "
-  if [[ "$special_chars" =~ "$argument" ]]; then
-    # echo "SUCCESS $argument is a special char"
-    return 0
-  else
-    # echo "ERROR $argument is not a special char"
-    return 1
-  fi
+    local argument="$1"
+    local special_chars="!@#$%^&*()_+-=[]{}|;:,.<>/?~'\"\\\ "
+    if [[ "$special_chars" =~ "$argument" ]]; then
+        # echo "SUCCESS $argument is a special char"
+        return 0
+    else
+        # echo "ERROR $argument is not a special char"
+        return 1
+    fi
 }
 
 function util::is_first_char_special() {
-  local argument="$1"
-  local first_char="${argument:0:1}"
-  if util::is_special_char "$first_char"; then
-    # echo "SUCCESS $first_char is a first special char"
-    return 0
-  else
-    # echo "ERROR $first_char is not a first special char"
-    return 1
-  fi
+    local argument="$1"
+    local first_char="${argument:0:1}"
+    if util::is_special_char "$first_char"; then
+        # echo "SUCCESS $first_char is a first special char"
+        return 0
+    else
+        # echo "ERROR $first_char is not a first special char"
+        return 1
+    fi
 }
 
 function util::is_hex() {
-  local argument="$1"
-  if [[ "$argument" =~ ^#[0-9a-fA-F]{6}$ ]]; then
-    # echo "SUCCESS $argument is a valid hex"
-    return 0
-  else
-    # echo "ERROR $argument is not a valid hex"
-    return 1
-  fi
+    local argument="$1"
+    if [[ "$argument" =~ ^#[0-9a-fA-F]{6}$ ]]; then
+        # echo "SUCCESS $argument is a valid hex"
+        return 0
+    else
+        # echo "ERROR $argument is not a valid hex"
+        return 1
+    fi
 }
 
 function util::contains_special_char() {
-  local argument="$1"
-  if [[ "$argument" =~ [^[:alnum:]_] ]]; then
-    # echo "SUCCESS $argument contains special char"
-    return 0
-  else
-    # echo "ERROR $argument is not contains special char"
-    return 1
-  fi
+    local argument="$1"
+    if [[ "$argument" =~ [^[:alnum:]_] ]]; then
+        # echo "SUCCESS $argument contains special char"
+        return 0
+    else
+        # echo "ERROR $argument is not contains special char"
+        return 1
+    fi
 }
 
 function util::one_line_progress() {
-  local command="$@"
-  ${command} 2>&1 | while IFS= read -r line; do
-    echo -n -e "${clean_line_seq}${line}"
-  done
+    local command="$@"
+    ${command} 2>&1 | while IFS= read -r line; do
+        echo -n -e "${clean_line_seq}${line}"
+    done
 }
