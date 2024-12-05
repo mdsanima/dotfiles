17c8d145ca39ce2e1bef8689d70ef8a5fced91fb
Marcin Różewski <marcinrozewski@gmail.com>
2024-12-05 02:45:08 +0100

    chore: Update comments and docs in Shell library (#150)

--- lib/mdsanima-shell/libevent.sh
+++ lib/mdsanima-shell/libevent.sh
@@ -1,28 +1,33 @@
-# Copyright (c) 2024 Marcin Różewski, MDSANIMA LAB. All rights reserved.
-# Licensed under the MIT license.
+# Copyright (c) 2024 Marcin Różewski, MDSANIMA LAB
 
 # Library for event logging functions.  This is still work in progress.
 
+# =====> LIBEVENT <============================================================
 function event::debug() {
     print::color -fg "${BLACK}" -bg "${GRAY}" -b " DEBUG "
 }
 
+# =====> LIBEVENT <============================================================
 function event::dev() {
     print::color -fg "${BLACK}" -bg "${BLUE}" -b " DEV "
 }
 
+# =====> LIBEVENT <============================================================
 function event::error() {
     print::color -fg "${BLACK}" -bg "${RED}" -b " ERROR "
 }
 
+# =====> LIBEVENT <============================================================
 function event::info() {
     print::color -fg "${BLACK}" -bg "${CYAN}" -b " INFO "
 }
 
+# =====> LIBEVENT <============================================================
 function event::success() {
     print::color -fg "${BLACK}" -bg "${LIME}" -b " SUCCESS "
 }
 
+# =====> LIBEVENT <============================================================
 function event::warning() {
     print::color -fg "${BLACK}" -bg "${ORANGE}" -b " WARNING "
 }
