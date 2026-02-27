800e6a2880fad19bc958ffd7a5f0ffa5b3ebf4d2
Marcin Różewski <marcinrozewski@gmail.com>
2026-02-28 00:44:45 +0100

    style: Remove first space on comments

--- lib/mdsanima-shell/libutil.sh
+++ lib/mdsanima-shell/libutil.sh
@@ -17,10 +17,10 @@ function util::is_package_installed() {
     local query
     query=$(dpkg-query -W -f='${Status}' "${package}" 2>/dev/null)
     if echo "${query}" | grep -q "install ok installed"; then
-        # echo "SUCCESS $package is installed"
+        #echo "SUCCESS $package is installed"
         return 0
     else
-        # echo "ERROR $package is not installed"
+        #echo "ERROR $package is not installed"
         return 1
     fi
 }
@@ -35,10 +35,10 @@ function util::is_installed() {
 function util::is_integer() {
     local argument="$1"
     if [[ "$argument" =~ ^[0-9]+$ ]]; then
-        # echo "SUCCESS $argument is a integer"
+        #echo "SUCCESS $argument is a integer"
         return 0
     else
-        # echo "ERROR $argument is not a integer"
+        #echo "ERROR $argument is not a integer"
         return 1
     fi
 }
@@ -47,10 +47,10 @@ function util::is_integer() {
 function util::is_float() {
     local argument="$1"
     if [[ "$argument" =~ ^[0-9]+\.[0-9]+$ ]]; then
-        # echo "SUCCESS $argument is a float"
+        #echo "SUCCESS $argument is a float"
         return 0
     else
-        # echo "ERROR $argument is not a float"
+        #echo "ERROR $argument is not a float"
         return 1
     fi
 }
@@ -59,10 +59,10 @@ function util::is_float() {
 function util::is_number() {
     local argument="$1"
     if util::is_integer "$argument" || util::is_float "$argument"; then
-        # echo "SUCCESS $argument is a number"
+        #echo "SUCCESS $argument is a number"
         return 0
     else
-        # echo "ERROR $argument is not a number"
+        #echo "ERROR $argument is not a number"
         return 1
     fi
 }
@@ -71,10 +71,10 @@ function util::is_number() {
 function util::is_string() {
     local argument="$1"
     if [[ "$argument" =~ ^[a-zA-Z]+$ ]]; then
-        # echo "SUCCESS $argument is a string"
+        #echo "SUCCESS $argument is a string"
         return 0
     else
-        # echo "ERROR $argument is not a string"
+        #echo "ERROR $argument is not a string"
         return 1
     fi
 }
@@ -84,10 +84,10 @@ function util::is_special_char() {
     local argument="$1"
     local special_chars="!@#$%^&*()_+-=[]{}|;:,.<>/?~'\"\\\ "
     if [[ "$special_chars" =~ $argument ]]; then
-        # echo "SUCCESS $argument is a special char"
+        #echo "SUCCESS $argument is a special char"
         return 0
     else
-        # echo "ERROR $argument is not a special char"
+        #echo "ERROR $argument is not a special char"
         return 1
     fi
 }
@@ -97,10 +97,10 @@ function util::is_first_char_special() {
     local argument="$1"
     local first_char="${argument:0:1}"
     if util::is_special_char "$first_char"; then
-        # echo "SUCCESS $first_char is a first special char"
+        #echo "SUCCESS $first_char is a first special char"
         return 0
     else
-        # echo "ERROR $first_char is not a first special char"
+        #echo "ERROR $first_char is not a first special char"
         return 1
     fi
 }
@@ -109,10 +109,10 @@ function util::is_first_char_special() {
 function util::is_hex() {
     local argument="$1"
     if [[ "$argument" =~ ^#[0-9a-fA-F]{6}$ ]]; then
-        # echo "SUCCESS $argument is a valid hex"
+        #echo "SUCCESS $argument is a valid hex"
         return 0
     else
-        # echo "ERROR $argument is not a valid hex"
+        #echo "ERROR $argument is not a valid hex"
         return 1
     fi
 }
@@ -121,10 +121,10 @@ function util::is_hex() {
 function util::contains_special_char() {
     local argument="$1"
     if [[ "$argument" =~ [^[:alnum:]_] ]]; then
-        # echo "SUCCESS $argument contains special char"
+        #echo "SUCCESS $argument contains special char"
         return 0
     else
-        # echo "ERROR $argument is not contains special char"
+        #echo "ERROR $argument is not contains special char"
         return 1
     fi
 }
